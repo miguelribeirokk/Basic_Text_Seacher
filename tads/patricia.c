a4ea2bd7fff44e994b7cc74cd060a2acfe2af17c
WhosVinicius <silvaviniciusgontijo@gmail.com>
2022-06-24 13:10:22 -0300

    hash na main

--- tads/patricia.c
+++ tads/patricia.c
@@ -3,12 +3,7 @@
 //Alan Gabriel Martins Silva - 4663
 //Vinicius Alves Gontijo - 4708
 //Gabriel Ryan -
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
 #include "patricia.h"
-#include "linked_list.h"
 
 
 int Inicializa(No_Patricia **no) { //Inicializa a arvore
