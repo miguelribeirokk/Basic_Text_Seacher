12d9b7458e7a15da45bc04aac22fc91d040b1017
WhosVinicius <silvaviniciusgontijo@gmail.com>
2022-06-25 20:32:33 -0300

    calcula palavras diferentes do texto

--- tads/hashAssets.c
+++ tads/hashAssets.c
@@ -96,23 +96,23 @@ void removerDaTabela(hashTable *hashtable, const char *key){
     int idx = 0;
 
     // walk through each entry until either the end is reached or a matching key is found
-    while (entry != NULL) {
+    while(entry != NULL){
         // check key
-        if (strcmp(entry->key, key) == 0) {
+        if(strcmp(entry->key, key) == 0){
             // first item and no next entry
-            if (entry->next == NULL && idx == 0) {
+            if (entry->next == NULL && idx == 0){
                 hashtable->entries[bucket] = NULL;
             }
             // first item with a next entry
-            if (entry->next != NULL && idx == 0) {
+            if (entry->next != NULL && idx == 0){
                 hashtable->entries[bucket] = entry->next;
             }
             // last item
-            if (entry->next == NULL && idx != 0) {
+            if (entry->next == NULL && idx != 0){
                 prev->next = NULL;
             }
             // middle item
-            if (entry->next != NULL && idx != 0) {
+            if (entry->next != NULL && idx != 0){
                 prev->next = entry->next;
             }
             // free the deleted entry
@@ -131,13 +131,13 @@ void removerDaTabela(hashTable *hashtable, const char *key){
 void printaTabela(hashTable *tabela){
     for(int i = 0; i < tabela->tamanho; i++) {
         tipoItem *entry = tabela->entries[i];
-        if(entry == NULL) {
+        if(entry == NULL){
             continue;
         }
         for(;;) {
             printf("%s", entry->key);
             LE_Printa_Lista(&(entry->lista));
-            if (entry->next == NULL) {
+            if (entry->next == NULL){
                 break;
             }
             entry = entry->next;
@@ -150,10 +150,10 @@ int calcPalavras(hashTable *tabela,int idDoArq){
     int cont = 0;
     for(int i = 0; i < tabela->tamanho; ++i) {
     tipoItem *entry = tabela->entries[i];
-    if(entry == NULL) {
+    if(entry == NULL){
         continue;
     }
-    for(;;) {
+    for(;;){
         if(entry->lista->nome_arquivo == idDoArq){
             cont++;
         }
@@ -166,11 +166,11 @@ int calcPalavras(hashTable *tabela,int idDoArq){
                 aux = aux->prox;
             }
         }
-        if (entry->next == NULL) {
+        if (entry->next == NULL){
             break;
         }
         entry = entry->next;
-    }
+        }
     }
     return cont;
 }
